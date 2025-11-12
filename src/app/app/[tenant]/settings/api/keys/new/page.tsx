1810e5c5906e6929a66acaf94a4e95ab0214fb3a
saashqdev <saashqdev@gmail.com>
2025-11-12 13:31:21 -0500

    more migrations

--- src/app/app/[tenant]/settings/api/keys/new/page.tsx
+++ src/app/app/[tenant]/settings/api/keys/new/page.tsx
@@ -33,7 +33,7 @@ export default function ApiNewKeyRoute() {
   return (
     <>
       <OpenModal className="sm:max-w-xl" onClose={() => router.push(UrlUtils.currentTenantUrl(params, "settings/api/keys"))}>
-        <ApiKeyForm entities={appData.entities} onSubmit={handleFormAction} />
+        <ApiKeyForm entities={appData?.entities || []} onSubmit={handleFormAction} />
         {actionData?.apiKey !== undefined && (
           <ApiKeyCreatedModal apiKey={actionData?.apiKey} redirectTo={UrlUtils.currentTenantUrl(params, "settings/api/keys")} />
         )}
