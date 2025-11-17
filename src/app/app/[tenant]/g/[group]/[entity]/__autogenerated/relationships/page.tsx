e94c6f96007d5174c9ff1243edba550c69b7c061
saashqdev <saashqdev@gmail.com>
2025-11-17 13:08:43 -0500

    more migrations

--- src/app/app/[tenant]/g/[group]/[entity]/__autogenerated/relationships/page.tsx
+++ src/app/app/[tenant]/g/[group]/[entity]/__autogenerated/relationships/page.tsx
@@ -1,15 +1,15 @@
 import ServerError from "@/components/ui/errors/ServerError";
-import { LoaderData, loader } from "@/modules/rows/routes/Rows_Relationships.server";
+import { LoaderData, loader as rowsRelationshipsLoader } from "@/modules/rows/routes/Rows_Relationships.server";
 import RowsRelationshipsRoute from "@/modules/rows/components/RowsRelationshipsRoute";
 import { serverTimingHeaders } from "@/modules/metrics/utils/defaultHeaders.server";
 import { IServerComponentsProps } from "@/lib/dtos/ServerComponentsProps";
 
 export { serverTimingHeaders as headers };
 
-export const loader = (props: IServerComponentsProps) => loader(props);
+export const loader = (props: IServerComponentsProps) => rowsRelationshipsLoader(props);
 
 export default async function Page(props: IServerComponentsProps) {
-  const response = await loader(props);
+  const response = await rowsRelationshipsLoader(props);
   const data = await response.json() as LoaderData;
   return <RowsRelationshipsRoute data={data} />;
 }
