5d440e11fc69dbac120df5c02e4ff88d96e2ec7e
saashqdev <saashqdev@gmail.com>
2025-11-21 14:49:49 -0500

    more migrations

---
+++ src/app/app/[tenant]/settings/api/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { redirect } from "next/navigation";
+import UrlUtils from "@/utils/app/UrlUtils";
+import { IServerComponentsProps } from "@/lib/dtos/ServerComponentsProps";
+
+export default async function ApiKeyRedirectRoute(props: IServerComponentsProps) {
+  const params = await props.params;
+  const id = params.id;
+  
+  // Redirect to the correct path with /keys/ in the URL
+  redirect(UrlUtils.currentTenantUrl(params, `settings/api/keys/${id}`));
+}
